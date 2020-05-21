d065eb6beda8f60cedca8e69f34dc81873888adc
Gustavo Rodrigues Mietlicki <gustavo.mietlicki@deal.br>
2020-05-21 15:56:33 -0300

    Adicionado componente para pesquisa de pessoas e menu intercambiavel

---
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css']
+})
+export class PessoasPesquisaComponent{
+
+}
